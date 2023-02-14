148f1f69955471febbbe6fcf86cb7a8c7c3a9572
ZL L <19965526036@163.com>
2023-02-15 01:23:43 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using Terraria;
+using TerrariaApi.Server;
+
+namespace CleanTheMoneyTakenByNPC
+{
+    [ApiVersion(2, 1)]
+    public partial class Class1 : TerrariaPlugin
+    {
+        public override string Author => "z枳";
+
+        public override string Description => "清空敌怪捡到的钱，避免原版刷钱bug";
+
+        public override string Name => "CleanTheMoneyTakenByNPC";
+
+        public override Version Version => new Version(1, 0, 0, 0);
+
+        public Class1(Main game) : base(game) { }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.NpcAIUpdate.Register(this, OnNPCUpdate);
+        }
+
+        private void OnNPCUpdate(NpcAiUpdateEventArgs args)
+        {
+            if(args.Npc.active && args.Npc.extraValue > 0)
+            {
+                args.Npc.extraValue = 0;
+                NetMessage.SendData(92, -1, -1, null, args.Npc.whoAmI, 0, args.Npc.position.X, args.Npc.position.Y, 0, 0, 0);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.NpcAIUpdate.Deregister(this, OnNPCUpdate);
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
